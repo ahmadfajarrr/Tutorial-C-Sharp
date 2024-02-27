70a9f790373ce14b7d10c30bd9721783aaf2342d
ahmadfajarrr <ahmadfajarpersonal@gmail.com>
2024-02-27 11:01:46 -0500

    Tutorial C# 1

---
+++ TutorialC#1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TutorialCSharp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello Word"); // menulis Kata hello word di console
+			Console.ReadLine(); //Menampilkan tulisan console yang telah di buat
+			//main idea
+		}
+	}
+}
